ea4db58980bc6feda9086fc86aca2e029c03f207
hbubante <hbubante@gmail.com>
2026-08-02 22:09:40 +0800

    Implemented about section

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,30 @@ import Button from "@mui/material/Button"
 import './App.css';
 import '@fontsource-variable/inter';
 
+import { SiJavascript, SiTypescript, SiPython, SiCplusplus, SiC, SiDotnet, SiPostgresql } from "react-icons/si";
+import { FaHtml5, FaCss3Alt, FaJava, FaReact } from "react-icons/fa";
+import Csharp from "./assets/csharp.png";
+import Cplusplus from "./assets/cplusplus.png";
+import C from "./assets/c.png";
+
 function App() {
 
     return (
         <main className="
         flex
         flex-col
+        justify-start
         items-center
+        gap-[300px]
+        pb-[200px]
         min-h-screen
         bg-background
         font
         ">
-            <div className="
+            <nav className="
+                fixed
+                top-0
+                z-50
                 flex
                 flex-row
                 justify-end
@@ -22,9 +34,7 @@ function App() {
                 w-full
                 px-[100px]
                 py-[50px]
-                border-1
-                border-solid
-                border-white
+                bg-background
             ">
                 <p className="
                 text-[32px]
@@ -38,7 +48,7 @@ function App() {
                 ">
                     About
                 </p>
-                
+
                 <p className="
                 text-[32px]
                 font-[600]
@@ -64,23 +74,21 @@ function App() {
                 ">
                     Contact
                 </p>
-            </div>
+            </nav>
 
             <div className="
             flex
             flex-col
             items-left
             gap-[50px]
+            mt-[200px]
             w-full
             max-w-[1280px]
-            border-1
-            border-solid
-            border-white
             ">
                 <h1 className="
                 accent-accent
                 ">
-                    hey there;
+                    hey there<span className="surface-surface">;</span>
                 </h1>
 
                 <p className="
@@ -106,6 +114,160 @@ function App() {
                 }}>
                     Contact Me
                 </Button>
+            </div>
+
+            <div className="
+            flex
+            flex-row
+            justify-between
+            items-stretch
+            gap-[50px]
+            w-full
+            max-w-[1280px]
+            ">
+                <div className="
+                    flex
+                    flex-col
+                    gap-[50px]
+                ">
+                    <h2 className="
+                    accent-accent
+                    ">
+                        about me<span className="surface-surface">;</span>
+                    </h2>
+
+                    <p className="
+                    max-w-[638px]
+                    text-[24px]
+                    font-[400]
+                    text-text
+                    ">
+                        I am a Computer Science graduate focusing on full-stack development and currently specializing in React and .NET, among other technologies. I have hands-on experience with systems development through internship, academic, and practical projects.
+                    </p>
+
+                    <div className="
+                        flex
+                        flex-row
+                        gap-[50px]
+                    ">
+                        <Button sx={{
+                        width: 200,
+                        height: 65,
+                        border: 2,
+                        borderColor: "#DCBAB5",
+                        borderRadius: 3,
+                        color: "#DCBAB5",
+                        fontFamily: "'Inter Variable', sans-serif",
+                        fontSize: 20,
+                        fontWeight: 600,
+                        textTransform: 'none'
+                        }}>
+                            View Resume
+                        </Button>
+
+                        <Button sx={{
+                        width: 200,
+                        height: 65,
+                        border: 2,
+                        borderColor: "#DCBAB5",
+                        borderRadius: 3,
+                        color: "#DCBAB5",
+                        fontFamily: "'Inter Variable', sans-serif",
+                        fontSize: 20,
+                        fontWeight: 600,
+                        textTransform: 'none'
+                        }}>
+                            View GitHub
+                        </Button>
+                    </div>
+                </div>
+
+                <div className="
+                    flex
+                    flex-col
+                    justify-center
+                    items-center
+                    gap-[50px]
+                ">
+                    <div className="
+                        flex
+                        flex-row
+                        items-center
+                        gap-[50px]
+                    ">
+                        <FaReact
+                        size={75}
+                        className="surface-surface"
+                        />
+
+                        <FaHtml5
+                        size={85}
+                        className="surface-surface"
+                        />
+
+                        <FaCss3Alt
+                        size={85}
+                        className="surface-surface"
+                        />
+
+                        <SiJavascript
+                        size={75}
+                        className="surface-surface"
+                        />
+
+                        <SiTypescript
+                        size={75}
+                        className="surface-surface"
+                        />
+                    </div>
+
+                    <div className="
+                        flex
+                        flex-row
+                        gap-[50px]
+                    ">
+                        <SiPython
+                        size={75}
+                        className="surface-surface"
+                        />
+
+                        <img 
+                            src={Csharp}
+                            style={{ width: "75px", height: "75px" }} 
+                        />
+
+                        <FaJava
+                        size={75}
+                        className="surface-surface"
+                        />
+
+                        <img 
+                            src={Cplusplus}
+                            style={{ width: "75px", height: "75px" }} 
+                        />
+
+                        <img 
+                            src={C}
+                            style={{ width: "75px", height: "75px" }} 
+                        />
+                    </div>
+
+                    <div className="
+                        flex
+                        flex-row
+                        gap-[50px]
+                    ">
+                        <SiDotnet
+                        size={75}
+                        className="surface-surface"
+                        />
+
+                        <SiPostgresql
+                        size={75}
+                        className="surface-surface"
+                        />
+                    </div>
+                </div>
             </div>
         </main>
     )
